ebff72e8a97696f9e1d1bbd6dbd386bb0046fbc9
Sergey <59853686+SergeyShakirov@users.noreply.github.com>
2021-03-14 01:27:27 +0600

    Project Django

--- Weather/WeatherNow/models.py
+++ Weather/WeatherNow/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Cities(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
--- Weather/WeatherNow/views.py
+++ Weather/WeatherNow/views.py
@@ -1,4 +1,15 @@
+import requests
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'WeatherNow/index.html')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c579b1fa460e36193367cf2b4bb6ed10'
+    city = 'London'
+    res = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon'],
+    }
+    context = {'info': city_info}
+    return render(request, 'WeatherNow/index.html', context)
